0db7d8ceb0e130ef00cfeda0ea3562c76655999e
Jamie Hollern <jamiehollern@gmail.com>
2020-05-04 12:28:31 +0100

    Updated lesson 4.

--- Lesson4/js/scripts.js
+++ Lesson4/js/scripts.js
@@ -1,1 +1,9 @@
-console.log('WomenWhoCode');
+var urlParams = new URLSearchParams(window.location.search);
+var name = urlParams.get('name') ? urlParams.get('name') : 'not set.';
+
+console.log(name);
+
+$('span.name').text(name);
+
+
+console.log(name);
